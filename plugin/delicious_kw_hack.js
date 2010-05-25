30fe546d9f9a1f17f7e27b7ef65db8769dbfcb4b
Massimo Lauria <lauria.massimo@gmail.com>
2010-05-25 08:22:09 +0200

    Initial COmmit of my configuration

---
+++ plugin/delicious_kw_hack.js
@@ -0,0 +1,19 @@
+bookmarks.getSearchURL = function(text, useDefsearch) {
+  let url = null;
+  let postData = {};
+  let searchString =
+    (useDefsearch ? options["defsearch"] + " " : "")
+    + text;
+
+  this.getSearchEngines();
+
+  url = window.getShortcutOrURI(searchString, postData);
+
+  if (url == searchString)
+    return null;
+
+  if (postData && postData.value)
+    return [url, postData.value];
+
+  return [url, null]; // can be null
+}
